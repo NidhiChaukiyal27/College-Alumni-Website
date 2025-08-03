5e889d3edae20575d2473189a63dbf99bb7641dd
NidhiChaukiyal27 <nidhichaukiyal789@gmail.com>
2025-08-03 14:43:46 +0530

    Switch to MongoDB Atlas for deployment

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,35 +1,19 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/studentsRegistration')
-const db = mongoose.connection
-db.once('open',()=>{
-    console.log("Mongodb connection successful")
-})
-
-// const mongoose = require("mongoose");
-
-// mongoose.connect("mongodb://localhost:27017/studentsRegistration", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
+// mongoose.connect('mongodb://127.0.0.1:27017/studentsRegistration')
+// const db = mongoose.connection
+// db.once('open',()=>{
+//     console.log("Mongodb connection successful")
 // })
-//   .then(() => {
-//     console.log("Database connection successful!");
-//   })
-//   .catch((err) => {
-//     console.error("Database connection error:", err);
-//   });
-
-    
-
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error: ", err));
 
 
-// const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://127.0.0.1:27017/studentsRegistration", {
-// }).then(() => {
-//     console.log("Connected to MongoDB");
-// }).catch((error) => {
-//     console.error("Error connecting to MongoDB:", error);
-// });
+
 
